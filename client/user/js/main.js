a218073b321c5045d7cc5cbc616799abd4a1f27b
Kirby Kohlmorgen <kirby@purdue.edu>
2014-06-21 04:31:55 -0700

    Add firebase request to ui

--- client/user/js/main.js
+++ client/user/js/main.js
@@ -1,14 +1,32 @@
 $(function() {
-  var initSelectionList = function(elem, list) {
-    $(elem).UISelectList({
-      selected: 2,
-      callback: function() {
-        $(list).html($(this).text());
-        console.log($(this).text());
-      }
-    });
-  }
-
   initSelectionList('#pickup-location .list', '#pickup-list-item');
   initSelectionList('#destination .list', '#destination-list-item');
+
+  $('#requestShuttle').on('click', function() {
+    var fbTripRequest = new Firebase('https://qualchat.firebaseio.com/trip_requests');
+    fbTripRequest.push({'destination':getListVal('#destination-list-item'),
+                          'start': getListVal('#pickup-list-item'),
+                          'target_time': '123456',
+                          'time_requested': unixTimeInSeconds()});
+    });
 });
+
+var initSelectionList = function(elem, list) {
+  $(elem).UISelectList({
+    callback: function() {
+      $(list).html($(this).text());
+    }
+  });
+}
+
+var unixTimeInSeconds = function() {
+  return Math.round(new Date().getTime() / 1000);
+}
+
+var getListVal = function(elem) {
+  var val = $(elem).html();
+  if(val.indexOf('Select') > -1) {
+    // alert('FUCK');
+  }
+  return val;
+}
