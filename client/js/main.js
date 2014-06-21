e22cdd8b8cf5d5fc0a219f3cf5e46d9f97ab7d5b
Kirby Kohlmorgen <kirby@purdue.edu>
2014-06-21 00:57:05 -0700

    Trying out angular

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,12 @@
+var x = 'hell';
+
 $(function() {
   var initSelectionList = function(elem) {
     $(elem).UISelectList({
       selected: 2,
       callback: function() {
         $("#response").html($(this).text());
+        x = $(this).text().replace(/\s/g, '');
       }
     });
 
@@ -17,3 +20,12 @@ $(function() {
   initSelectionList('#pickup-location .list');
   initSelectionList('#destination .list');
 });
+
+angular.module('root', [])
+  .controller('app', ['$scope', function($scope){
+    $scope.$watch('x', function(n) {
+      $scope.pickupLocation = n;
+    });
+
+    $scope.pickupLocation = x;
+  }]);
